e4841db656c7f89ae06dbeacefbb639ccc8693bb
sandzu <a.schaefer1995@gmail.com>
2018-05-09 21:58:44 -0700

    add W4D4 Hw

---
+++ W4D4/app/mailers/application_mailer.rb
@@ -0,0 +1,9 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "everybody@appacademy.io"
+  layout 'mailer'
+    def welcome_email(user)
+      @user = user
+      @url  = 'http://example.com/session/new'
+      mail(to: user.username, subject: 'Welcome to 99 Cats')
+  end
+end
